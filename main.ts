c9aa92a061ccae856f0f64a9f6d9b2d82fe110e2
sukandajong <sukandajong@gmail.com>
2026-07-21 20:05:24 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,14 +4,15 @@ basic.forever(function () {
     moisture = smarthome.ReadSoilHumidity(AnalogPin.P1)
     OLED.clear()
     OLED.writeStringNewLine("Moisture:")
-    OLED.writeNumNewLine(smarthome.ReadSoilHumidity(AnalogPin.P1))
-    if (smarthome.ReadSoilHumidity(AnalogPin.P1) < 40) {
+    OLED.writeNumNewLine(moisture)
+    if (moisture < 40) {
         basic.showIcon(IconNames.Sad)
         OLED.writeStringNewLine("Pump: ON")
-        smarthome.Relay(DigitalPin.P15, smarthome.RelayStateList.Off)
-    } else {
+        smarthome.Relay(DigitalPin.P15, smarthome.RelayStateList.On)
+    } else if (moisture >= 40) {
         basic.showIcon(IconNames.Happy)
         OLED.writeStringNewLine("Pump: OFF")
+        smarthome.Relay(DigitalPin.P15, smarthome.RelayStateList.Off)
     }
-    basic.pause(500)
+    basic.pause(100)
 })
